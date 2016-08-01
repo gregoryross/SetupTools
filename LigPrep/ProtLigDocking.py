ac735b677974c7a4e40a939a692bd81cb9d0b5b8
Gregory Ross <gregoryross.uk@gmail.com>
2016-08-01 15:24:41 -0400

    Docking with AutoDock Vina fixed and documented

--- LigPrep/ProtLigDocking.py
+++ LigPrep/ProtLigDocking.py
@@ -1,6 +1,34 @@
+def DockLig_vina(lig_in, prot_in, center=None,size=None, outname = 'docklings',cleanfiles=True):
+    """
+    Wrapper function to flexibly dock small molecules to a user define volume in a protein using Autodock Vina.
+    Input files are automatically converted to Autodock's pdbqt format. It is recommended that the protein and ligand
+    have been correctly protonated.
 
+    The accepted fileformats of the the protein and ligand are all of those accepted by Openbabel.
 
-def DockLig_vina(lig_in, prot_in, center=None,size=None, outname = 'docklings',cleanfiles=False):
+    Example
+    --------
+    DockLig_vina(ligand.mol2, protein.pdb, center = [3.3,4.3,23.7], cleanfiles=False)
+
+    Parameters
+    ----------
+    lig_in : str
+        The filename of ligand--including file extension (suffix)--you wish to dock, including file extension (suffix)
+    prot_in: str
+        The filename of the protein--including file extension (suffix)-- that the ligand will be docked into.
+    center: list of floats
+        The center of the docking search space in the form [x,y,z], where x, y, and z are in Angstroms
+    size: list of floats
+        The lengths of the cuboid search volume in the form [x,y,z], where x, y, and z are in Angstroms
+    outname: str
+        The naming scheme the docked configurations and log file. Do not include the file extension (suffix)
+    cleanfiles: bool
+        Whether to remove the intermediate pdbqt files that were necessary for AutoDock Vina
+
+    Returns
+    -------
+    Docked structures (.pdbqt) and log file (.log) are written to the working directory
+    """
 
     if center == None:
         print 'Fatal: must specify the center of the search space'
@@ -9,6 +37,11 @@ def DockLig_vina(lig_in, prot_in, center=None,size=None, outname = 'docklings',c
         size = [15,15,15]
 
     import subprocess
+
+    # Checking whether the executable of Vina can be found
+    if subprocess.call('vinaa',shell=True) != 1:
+        print 'Autodock Vina not found. Cancelling docking.'
+        return
 
     # Getting the file types of the ligand and receptor
     ligname = lig_in.split('.')[0]
@@ -21,12 +54,12 @@ def DockLig_vina(lig_in, prot_in, center=None,size=None, outname = 'docklings',c
     subprocess.call(cmd,shell=True)
 
     # Generate the pdbqt file for the protein
-    cmd = 'obabel -i{0} {1} -o pdbqt -rx -O {2}.pdbqt'.format(protype,prot_in,protname)
+    cmd = 'obabel -i{0} {1} -o pdbqt -xr -O {2}.pdbqt'.format(protype,prot_in,protname)
     subprocess.call(cmd,shell=True)
 
     # Run AutoDock
-    cmd = 'vina --receptor {0}.pdbqt --ligand {1}.pdbqt --center_x {2} --center_y {3} --center_z {4}' \
-          ' --size_x {5} --size_y {6} --size_z {7} --out {8}.pdbqt --log {9}.log'.format(protname,ligname,center[0],center[1],center[2],size[0],size[1],size[2],outname,outname)
+    cmd = 'vina --receptor {0}.pdbqt --ligand {1}.pdbqt --center_x {2} --center_y {3} --center_z {4} --size_x {5} --size_y {6} --size_z {7} --out {8}.pdbqt --log {9}.log'.format(protname,ligname,center[0],center[1],center[2],size[0],size[1],size[2],outname,outname)
+    print cmd
     subprocess.call(cmd,shell=True)
 
     if cleanfiles == True:
